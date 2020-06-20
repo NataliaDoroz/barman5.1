a72862b4f2ce0f1a5e6be34739c2ef7b5047302b
NataliaD <natalia123doroz@gmail.com>
2020-06-20 15:21:18 +0200

    .

--- src/Barman.java
+++ src/Barman.java
@@ -2,7 +2,7 @@ public class Barman {
 
     public void order(Person person1, Drink drink1) {
         if (person1.getAge() < 18) {
-            if (drink1.isAlcohol() == true) {
+            if (drink1.isAlcohol()) {
                 System.out.println(person1.getFirstName() + ", Masz mniej niż 18 lat. Odmowa sprzedaży alkoholu");
             } else {
                 System.out.println(person1.getFirstName() + ", dla Ciebie " + drink1.getName() + ". Kwota do zapłaty: " + drink1.getPrice() + "zł");
