c772093fba9f137573fb305198616a89ff8160ca
NataliaD <natalia123doroz@gmail.com>
2020-06-20 17:47:43 +0200

    change object names

--- src/Barman.java
+++ src/Barman.java
@@ -1,10 +1,10 @@
 public class Barman {
 
-    public void order(Person person1, Drink drink1) {
-        if (person1.getAge() < 18 && (drink1.isAlcohol())) {
-            System.out.println(person1.getFirstName() + ", Masz mniej niż 18 lat. Odmowa sprzedaży alkoholu");
+    public void order(Person person, Drink drink) {
+        if (person.getAge() < 18 && (drink.isAlcohol())) {
+            System.out.println(person.getFirstName() + ", Masz mniej niż 18 lat. Odmowa sprzedaży alkoholu");
         } else {
-            System.out.println(person1.getFirstName() + ", Twój drink jest gotowy do odbioru. Kwota do zapłaty: " + drink1.getPrice() + "zł");
+            System.out.println(person.getFirstName() + ", Twój drink jest gotowy do odbioru. Kwota do zapłaty: " + drink.getPrice() + "zł");
         }
 
     }
